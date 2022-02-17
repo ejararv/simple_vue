0ee87df0e93718a13d65b9b2fdb8f9dd67c431d6
Vladimir Reshetsko <44954340+ejararv@users.noreply.github.com>
2022-02-17 14:59:00 +0100

    2-way binding, modificators, sync data with inputs, @submit.prevent

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,99 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
-</template>
+  <div class="app">
+    <div>
+      <form @submit.prevent>
+        <h4>Create post</h4>
+        <input
+          v-bind:value="title"
+          @input="title = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Title"
+        />
+        <input
+          v-bind:value="body"
+          @input="body = $event.target.value"
+          class="input"
+          type="text"
+          placeholder="Text"
+        />
+        <button class="btn" @click="createPost">Create</button>
+      </form>
+    </div>
+    <div class="post" v-for="post in posts" :key="post.id">
+      <div><strong>Name:</strong> {{ post.title }}</div>
+      <div><strong>Details:</strong> {{ post.body }}</div>
+    </div>
+  </div>
+</template> 
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: "JavaScript 1",
+          body: "Text 1",
+        },
+        {
+          id: 2,
+          title: "JavaScript 2",
+          body: "Text 2",
+        },
+        {
+          id: 3,
+          title: "JavaScript 3",
+          body: "Text 3",
+        },
+      ],
+      title: "",
+      body: "",
+    };
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(newPost);
+    },
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.post {
+  padding: 15px;
+  border: 1px solid grey;
+  margin-top: 15px;
+}
+.input {
+  width: 100%;
+  border: 1px solid grey;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  border: 1px solid grey;
 }
 </style>
